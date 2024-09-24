358bcedfd838e033f93b7886f8ea243a62c55d4a
seanyoo0405 <148388939+seanyoo0405@users.noreply.github.com>
2024-09-24 18:27:04 -0400

    account, courses, dashboard

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,15 +1,31 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+import Courses from "./Courses";
 import Account from "./Account";
 import Signin from "./Account/Signin";
+import Dashboard from "./Dashboard";
+import KanbasNavigation from "./Navigation";
 
 export default function Kanbas() {
     return (
         <div id="wd-kanbas">
-            <h1>Kanbas</h1>
-            <Routes>
-        <Route path="/" element={<Navigate to="Account" />} />
-        <Route path="/Account/*" element={<Account />} />
-      </Routes>
-        </div>
+            <table>
+                <tr>
+                    <td valign="top">
+                        <KanbasNavigation />
+                    </td>
+                    <td valign="top">
+                        <Routes>
+                            <Route path="/" element={<Navigate to="Account" />} />
+                            <Route path="/Account/*" element={<Account />} />
+                            <Route path="/Dashboard" element={<Dashboard />} />
+                            <Route path="/Courses" element={<Dashboard />} />
+                            <Route path="/Courses/:cid/*" element={<Courses />} />
+                            <Route path="/Calendar" element={<h1>Calendar</h1>} />
+                            <Route path="/Inbox" element={<h1>Inbox</h1>} />
+                        </Routes>
+                    </td>
+                </tr>
+            </table>
+        </div >
     );
 }
